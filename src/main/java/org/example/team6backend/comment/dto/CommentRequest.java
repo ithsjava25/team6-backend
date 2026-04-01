3c45b933a7672ac476a6ac38f353bb79965a6964
Edvin Karlsson <edvinkarlsson4@gmail.com>
2026-04-01 11:13:47 +0200

    Added CommentController and CommentRequest DTO for creating and fetching comments

---
+++ src/main/java/org/example/team6backend/comment/dto/CommentRequest.java
@@ -0,0 +1,32 @@
+package org.example.team6backend.comment.dto;
+
+public class CommentRequest {
+
+    private Long incidentId;
+    private String userId;
+    private String message;
+
+    public Long getIncidentId() {
+        return incidentId;
+    }
+
+    public void setIncidentId(Long incidentId) {
+        this.incidentId = incidentId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
