6ff0a5204865d59299546c6c8dfe4e4f282cf4ba
Edvin Karlsson <edvinkarlsson4@gmail.com>
2026-04-01 11:51:32 +0200

    Changed to ResourceNotFoundException

--- src/main/java/org/example/team6backend/comment/service/CommentService.java
+++ src/main/java/org/example/team6backend/comment/service/CommentService.java
@@ -2,6 +2,7 @@ package org.example.team6backend.comment.service;
 
 import org.example.team6backend.comment.entity.Comment;
 import org.example.team6backend.comment.repository.CommentRepository;
+import org.example.team6backend.exception.ResourceNotFoundException;
 import org.example.team6backend.incident.entity.Incident;
 import org.example.team6backend.incident.repository.IncidentRepository;
 import org.example.team6backend.user.entity.AppUser;
@@ -31,10 +32,10 @@ public class CommentService {
 
     public Comment createComment(Long incidentId, String userId, String message) {
         Incident incident = incidentRepository.findById(incidentId)
-                .orElseThrow(() -> new RuntimeException("Incident not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Incident not found"));
 
         AppUser user = appUserRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
         Comment comment = new Comment();
         comment.setIncident(incident);
