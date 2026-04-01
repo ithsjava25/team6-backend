dc7a0a6fd872f88b294f8c61174f9f6b1464740a
Edvin Karlsson <edvinkarlsson4@gmail.com>
2026-04-01 10:54:01 +0200

    Added CommentRepository and CommentService with basic logic

---
+++ src/main/java/org/example/team6backend/comment/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package org.example.team6backend.comment.repository;
+
+import org.example.team6backend.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, String> {
+
+    List<Comment> findByIncidentId(Long incidentId);
+}
