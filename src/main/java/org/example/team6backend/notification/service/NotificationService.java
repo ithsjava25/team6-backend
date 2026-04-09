d7a75ef55fa483d30f9d79a0f6b4b762783dc546
Edvin Karlsson <edvinkarlsson4@gmail.com>
2026-04-09 15:46:26 +0200

    added incident to notification so it saves correctly

--- src/main/java/org/example/team6backend/notification/service/NotificationService.java
+++ src/main/java/org/example/team6backend/notification/service/NotificationService.java
@@ -7,7 +7,6 @@ import org.example.team6backend.user.entity.AppUser;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class NotificationService {
