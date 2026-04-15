f2dbc2f0b4d94349a719570d831a453ed809d33f
Edvin Karlsson <edvinkarlsson4@gmail.com>
2026-04-15 12:08:51 +0200

    refactor: store timestamp in UTC using Instant and timestampz

--- src/main/java/org/example/team6backend/comment/controller/CommentController.java
+++ src/main/java/org/example/team6backend/comment/controller/CommentController.java
@@ -3,7 +3,6 @@ package org.example.team6backend.comment.controller;
 import jakarta.validation.Valid;
 import org.example.team6backend.comment.dto.CommentRequest;
 import org.example.team6backend.comment.dto.CommentResponse;
-import org.example.team6backend.comment.entity.Comment;
 import org.example.team6backend.comment.service.CommentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -24,10 +23,8 @@ public class CommentController {
 	@GetMapping("/incident/{incidentId}")
 	@ResponseBody
 	public ResponseEntity<List<CommentResponse>> getCommentByIncidentId(@PathVariable Long incidentId) {
-		List<CommentResponse> comments = commentService.getCommentByIncidentId(incidentId)
-				.stream()
-				.map(CommentResponse::fromEntity)
-				.toList();
+		List<CommentResponse> comments = commentService.getCommentByIncidentId(incidentId).stream()
+				.map(CommentResponse::fromEntity).toList();
 
 		return ResponseEntity.ok(comments);
 	}
--- src/main/java/org/example/team6backend/comment/dto/CommentResponse.java
+++ src/main/java/org/example/team6backend/comment/dto/CommentResponse.java
@@ -4,32 +4,17 @@ import org.example.team6backend.comment.entity.Comment;
 
 import java.time.Instant;
 
-public record CommentResponse(
-        String id,
-        String message,
-        Instant createdAt,
-        CommentUserResponse user
+public record CommentResponse(String id, String message, Instant createdAt, CommentUserResponse user
 
 ) {
-    public static CommentResponse fromEntity(Comment comment) {
-        return new CommentResponse(
-                comment.getId(),
-                comment.getMessage(),
-                comment.getCreatedAt(),
-                comment.getUser() != null
-                        ? new CommentUserResponse(
-                        comment.getUser().getId(),
-                        comment.getUser().getName(),
-                        comment.getUser().getEmail()
-                )
-                        : null
-        );
-    }
+	public static CommentResponse fromEntity(Comment comment) {
+		return new CommentResponse(comment.getId(), comment.getMessage(), comment.getCreatedAt(),
+				comment.getUser() != null
+						? new CommentUserResponse(comment.getUser().getId(), comment.getUser().getName(),
+								comment.getUser().getEmail())
+						: null);
+	}
 
-    public record CommentUserResponse(
-            String id,
-            String name,
-            String email
-    ) {
-    }
+	public record CommentUserResponse(String id, String name, String email) {
+	}
 }
